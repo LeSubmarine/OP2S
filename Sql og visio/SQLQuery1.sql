cee5f0685c4ef919a8110423b4c25c6662e641ab
LeSubmarine <henrik@osterby.dk>
2020-04-20 11:51:12 +0200

    BRrrt

--- Sql og visio/SQLQuery1.sql
+++ Sql og visio/SQLQuery1.sql
@@ -1,5 +1,34 @@
 ﻿CREATE TABLE Facility(
 Facility_No int NOT NULL PRIMARY KEY,
 Types varchar(5) NOT NULL);
+
+CREATE TABLE Hotel(
+Hotel_No int NOT NULL PRIMARY KEY,
+Name varchar(50) NOT NULL,
+Address varchar(50) NOT NULL);
+
+CREATE TABLE Guest(
+Guest_No int NOT NULL PRIMARY KEY,
+Name varchar(50) NOT NULL,
+Address varchar(50) NOT NULL);
+
 CREATE TABLE HotelUseOfFacilities(
-Hotel_Facility_N
+Hotel_Facility_No int NOT NULL PRIMARY KEY,
+Hotel_No int NOT NULL FOREIGN KEY REFERENCES Hotel(Hotel_No),
+Facility_No int NOT NULL FOREIGN KEY REFERENCES Facility(Facility_No),
+Location varchar(50) NOT NULL
+);
+
+CREATE TABLE Room(
+Room_No int NOT NULL PRIMARY KEY,
+Hotel_No int NOT NULL FOREIGN KEY REFERENCES Hotel(Hotel_No),
+Types varchar(1) NOT NULL,
+Price int NOT NULL);
+
+CREATE TABLE Booking(
+Booking_No int NOT NULL PRIMARY KEY,
+Hotel_No int NOT NULL FOREIGN KEY REFERENCES Hotel(Hotel_No),
+Guest_No int NOT NULL FOREIGN KEY REFERENCES Guest(Guest_No),
+Room_No int NOT NULL,
+Date_From DateTime NOT NULL);
+
