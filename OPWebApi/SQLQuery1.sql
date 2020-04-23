8d417d546059a5fdfbf2eab1705c305705680563
Mike Sabroe Nielsen <mikesabroe@gmail.com>
2020-04-23 11:48:21 +0200

    Create SQLQuery1.sql

---
+++ OPWebApi/SQLQuery1.sql
@@ -0,0 +1,182 @@
+﻿INSERT INTO Hotel VALUES (1,'The Pope','Vaticanstreet 1 1111 Bishopcity'); 
+INSERT INTO Hotel VALUES (2,'Lucky Star','Lucky Road 12 2222 Hometown'); 
+INSERT INTO Hotel VALUES (3,'Discount','Inexpensive Road 7 3333 Cheaptown'); 
+INSERT INTO Hotel VALUES (4,'deLuxeCapital','Luxury Road 99 4444 Luxus'); 
+INSERT INTO Hotel VALUES (5,'Discount','Inexpensive Street 12 6666 Pricetown'); 
+INSERT INTO Hotel VALUES (6,'Prindsen','Algade 5, 4000 Roskilde'); 
+INSERT INTO Hotel VALUES (7,'Scandic','Sdr. Ringvej 5, 4000 Roskilde');
+INSERT INTO Room VALUES (1 ,1,'D', 1,200); 
+INSERT INTO Room VALUES (2 ,1,'D', 2,200); 
+INSERT INTO Room VALUES (3 ,1,'D', 3,200); 
+INSERT INTO Room VALUES (4 ,1,'D', 4,200); 
+INSERT INTO Room VALUES (5 ,1,'S',11,150); 
+INSERT INTO Room VALUES (6 ,1,'S',12,150); 
+INSERT INTO Room VALUES (7 ,1,'S',13,150); 
+INSERT INTO Room VALUES (8 ,1,'F',21,220); 
+INSERT INTO Room VALUES (9 ,1,'F',22,220); 
+INSERT INTO Room VALUES (10,1,'F',23,220); 
+INSERT INTO Room VALUES (11,2,'D', 1,230); 
+INSERT INTO Room VALUES (12,2,'D', 2,230); 
+INSERT INTO Room VALUES (13,2,'D', 3,230); 
+INSERT INTO Room VALUES (14,2,'D', 4,230); 
+INSERT INTO Room VALUES (15,2,'S',11,180); 
+INSERT INTO Room VALUES (16,2,'S',12,180); 
+INSERT INTO Room VALUES (17,2,'F',21,300); 
+INSERT INTO Room VALUES (18,2,'F',22,300); 
+INSERT INTO Room VALUES (19,3,'D', 1,175); 
+INSERT INTO Room VALUES (20,3,'D', 2,180); 
+INSERT INTO Room VALUES (21,3,'S',11,100); 
+INSERT INTO Room VALUES (22,3,'S',21,100); 
+INSERT INTO Room VALUES (23,3,'F',31,200); 
+INSERT INTO Room VALUES (24,3,'F',32,230); 
+INSERT INTO Room VALUES (25,4,'D', 1,500); 
+INSERT INTO Room VALUES (26,4,'D', 2,550); 
+INSERT INTO Room VALUES (27,4,'D', 3,550); 
+INSERT INTO Room VALUES (28,4,'S',11,350); 
+INSERT INTO Room VALUES (29,4,'S',12,360); 
+INSERT INTO Room VALUES (30,5,'D', 1,250); 
+INSERT INTO Room VALUES (31,5,'D', 2,170); 
+INSERT INTO Room VALUES (32,5,'S',11,150); 
+INSERT INTO Room VALUES (33,5,'F',21,300); 
+INSERT INTO Room VALUES (34,5,'F',22,310); 
+INSERT INTO Room VALUES (35,5,'F',23,320); 
+INSERT INTO Room VALUES (36,5,'F',24,320); 
+INSERT INTO Room VALUES (37,6,'D', 1,290); 
+INSERT INTO Room VALUES (38,6,'S',11,185); 
+INSERT INTO Room VALUES (39,6,'F',21,360); 
+INSERT INTO Room VALUES (40,6,'F',22,370); 
+INSERT INTO Room VALUES (41,6,'F',23,380); 
+INSERT INTO Room VALUES (42,6,'F',24,380); 
+INSERT INTO Room VALUES (43,7,'D', 1,200); 
+INSERT INTO Room VALUES (44,7,'D', 2,200); 
+INSERT INTO Room VALUES (45,7,'D', 3,200); 
+INSERT INTO Room VALUES (46,7,'D', 4,200); 
+INSERT INTO Room VALUES (47,7,'S',11,150); 
+INSERT INTO Room VALUES (48,7,'S',12,150); 
+INSERT INTO Room VALUES (49,7,'S',13,150); 
+INSERT INTO Room VALUES (50,7,'S',14,150); 
+INSERT INTO Room VALUES (51,7,'F',21,220); 
+INSERT INTO Room VALUES (52,7,'F',22,220); 
+INSERT INTO Room VALUES (53,7,'F',23,220); 
+INSERT INTO Room VALUES (54,7,'F',24,220); 
+INSERT INTO Guest VALUES( 1,'Eva' ,'Paradisvej 3, 1111 Bispeborg'); 
+INSERT INTO Guest VALUES( 2,'Adam' ,'Paradisvej 7, 1111 Bispeborg'); 
+INSERT INTO Guest VALUES( 3,'Goeg' ,'Sunset Blvd. 8, 2222 Hjemby'); 
+INSERT INTO Guest VALUES( 4,'Gokke' ,'Sunset Blvd. 8, 2222 Hjemby'); 
+INSERT INTO Guest VALUES( 5,'Fy' ,'Klovnevej 87, 3333 Lilleby'); 
+INSERT INTO Guest VALUES( 6,'Bi' ,'Bredgade 198, 3333 Lilleby'); 
+INSERT INTO Guest VALUES( 7,'Romeo' ,'Kaerlighedstunellen 1, 4444 Borgerslev'); 
+INSERT INTO Guest VALUES( 8,'Julie' ,'Kaerlighedstunellen 2, 4444 Borgerslev'); 
+INSERT INTO Guest VALUES( 9,'Godzilla','Dommervænget 16A, 4000 Roskilde'); 
+INSERT INTO Guest VALUES(10,'KingKong','Hyrdevænget 38, 4000 Roskilde'); 
+INSERT INTO Guest VALUES(11,'KongHans','Algade 10, 4000 Roskilde'); 
+INSERT INTO Guest VALUES(12,'Hans' ,'Vikingevej 45, 4000 Roskilde'); 
+INSERT INTO Guest VALUES(13,'Poul' ,'Domkirkevej 12, 4000 Roskilde'); 
+INSERT INTO Guest VALUES(14,'Erik' ,'Hestetorvet 8, 4000 Roskilde'); 
+INSERT INTO Guest VALUES(15,'Ulla' ,'Stændertorvet 4, 4000 Roskilde'); 
+INSERT INTO Guest VALUES(16,'Yrsa' ,'Sdr. Ringvej 21, 4000 Roskilde'); 
+INSERT INTO Guest VALUES(17,'Yvonne' ,'Østre Ringvej 12, 4000 Roskilde'); 
+INSERT INTO Guest VALUES(18,'Tim' ,'Ringstedgade 33, 4000 Roskilde'); 
+INSERT INTO Guest VALUES(19,'Sten' ,'Ringstedvej 23, 4000 Roskilde'); 
+INSERT INTO Guest VALUES(20,'Erland' ,'Skovbovængets alle 3, 4000 Roskilde'); 
+INSERT INTO Guest VALUES(21,'Erwin' ,'Ternevej 17, 4000 Roskilde'); 
+INSERT INTO Guest VALUES(22,'Åge' ,'Solsortevej 9, 4000 Roskilde'); 
+INSERT INTO Guest VALUES(23,'Åse' ,'Gyvelvej 45, 4000 Roskilde'); 
+INSERT INTO Guest VALUES(24,'Frede' ,'Københavnsvej 25, 4000 Roskilde'); 
+INSERT INTO Guest VALUES(25,'Palle' ,'Sct Ohlsgade 10, 4000 Roskilde'); 
+INSERT INTO Guest VALUES(26,'Jørn' ,'Dronning Amaliesvej 22, 4000 Roskilde'); 
+INSERT INTO Guest VALUES(27,'Stefan' ,'Lærkevej 65, 4000 Roskilde'); 
+INSERT INTO Guest VALUES(28,'John' ,'By03ken 56, 4000 Roskilde'); 
+INSERT INTO Guest VALUES(29,'Dana' ,'Byageren 12, 4000 Roskilde'); 
+INSERT INTO Guest VALUES(30,'Arn' ,'Vindingevej 23, 4000 Roskilde'); 
+INSERT INTO Booking VALUES( 1,1, 4,11,'2020-02-02','2020-02-06'); 
+INSERT INTO Booking VALUES( 2,1, 3,22,'2020-04-04','2020-04-06'); 
+INSERT INTO Booking VALUES( 3,2, 3,11,'2020-03-18','2020-03-22'); 
+INSERT INTO Booking VALUES( 4,3, 1,31,'2020-05-22','2020-05-28'); 
+INSERT INTO Booking VALUES( 5,3,10,21,'2020-02-04','2020-02-12'); 
+INSERT INTO Booking VALUES( 6,4, 2, 3,'2020-02-02','2020-02-06'); 
+INSERT INTO Booking VALUES( 7,4, 2, 3,'2020-04-20','2020-04-24'); 
+INSERT INTO Booking VALUES( 8,4, 7, 2,'2020-04-19','2020-04-24'); 
+INSERT INTO Booking VALUES( 9,4, 8,12,'2020-02-28','2020-03-05'); 
+INSERT INTO Booking VALUES(10,4, 5, 1,'2020-03-17','2020-03-20'); 
+INSERT INTO Booking VALUES(11,4, 6,11,'2020-04-11','2020-04-12'); 
+INSERT INTO Booking VALUES(12,5, 2,11,'2020-02-02','2020-02-06'); 
+INSERT INTO Booking VALUES(13,5, 2,21,'2020-04-11','2020-04-14'); 
+INSERT INTO Booking VALUES(14,5, 7, 2,'2020-04-16','2020-04-21'); 
+INSERT INTO Booking VALUES(15,6, 8,22,'2020-02-28','2020-03-03'); 
+INSERT INTO Booking VALUES(16,6, 5, 1,'2020-03-07','2020-03-10'); 
+INSERT INTO Booking VALUES(17,6, 6,11,'2020-04-02','2020-04-06'); 
+INSERT INTO Booking VALUES(18,6,12,21,'2020-02-02','2020-02-07'); 
+INSERT INTO Booking VALUES(19,6,13,22,'2020-02-04','2020-02-08'); 
+INSERT INTO Booking VALUES(20,6,14,23,'2020-02-05','2020-02-09'); 
+INSERT INTO Booking VALUES(21,6,21, 1,'2020-02-06','2020-02-10'); 
+INSERT INTO Booking VALUES(22,6,22,11,'2020-02-07','2020-02-11'); 
+INSERT INTO Booking VALUES(23,6,23,21,'2020-02-08','2020-02-12'); 
+INSERT INTO Booking VALUES(24,6,26,22,'2020-02-09','2020-02-13'); 
+INSERT INTO Booking VALUES(25,6,27,23,'2020-02-10','2020-02-14'); 
+INSERT INTO Booking VALUES(26,6,30,24,'2020-02-11','2020-02-15'); 
+INSERT INTO Booking VALUES(27,6,15, 1,'2020-02-12','2020-02-16'); 
+INSERT INTO Booking VALUES(28,6,16,11,'2020-02-13','2020-02-17'); 
+INSERT INTO Booking VALUES(29,6,17,21,'2020-02-14','2020-02-18'); 
+INSERT INTO Booking VALUES(30,7, 1, 1,'2020-02-02','2020-02-06'); 
+INSERT INTO Booking VALUES(31,7, 2, 2,'2020-02-03','2020-02-07'); 
+INSERT INTO Booking VALUES(32,7, 3, 3,'2020-02-04','2020-02-08'); 
+INSERT INTO Booking VALUES(33,7, 4, 4,'2020-02-05','2020-02-09'); 
+INSERT INTO Booking VALUES(34,7, 5,11,'2020-02-07','2020-02-11'); 
+INSERT INTO Booking VALUES(35,7, 6,21,'2020-02-08','2020-02-12'); 
+INSERT INTO Booking VALUES(36,7, 7,22,'2020-02-09','2020-02-13'); 
+INSERT INTO Booking VALUES(37,7, 8,23,'2020-02-10','2020-02-14'); 
+INSERT INTO Booking VALUES(38,7, 9,24,'2020-02-11','2020-02-15'); 
+INSERT INTO Booking VALUES(39,7,10, 1,'2020-02-12','2020-02-16'); 
+INSERT INTO Booking VALUES(40,7,11,11,'2020-02-13','2020-02-17'); 
+INSERT INTO Booking VALUES(41,7,12,21,'2020-02-14','2020-02-18'); 
+INSERT INTO Booking VALUES(42,7,13, 1,'2020-02-15','2020-02-16'); 
+INSERT INTO Booking VALUES(43,7,14, 2,'2020-02-16','2020-02-17'); 
+INSERT INTO Booking VALUES(44,7,15, 3,'2020-02-17','2020-02-18'); 
+INSERT INTO Booking VALUES(45,7,16, 4,'2020-02-18','2020-02-19'); 
+INSERT INTO Booking VALUES(46,7,17,11,'2020-02-19','2020-02-20'); 
+INSERT INTO Booking VALUES(47,7,18,21,'2020-02-20','2020-02-21'); 
+INSERT INTO Booking VALUES(48,7,19,22,'2020-02-21','2020-02-22'); 
+INSERT INTO Booking VALUES(49,7,20,23,'2020-02-22','2020-02-23'); 
+INSERT INTO Booking VALUES(50,7,21,24,'2020-02-23','2020-02-24'); 
+INSERT INTO Booking VALUES(51,7,22, 1,'2020-02-24','2020-02-25'); 
+INSERT INTO Booking VALUES(52,7,23,11,'2020-02-25','2020-02-26'); 
+INSERT INTO Booking VALUES(53,7,24,21,'2020-02-26','2020-02-28'); 
+INSERT INTO Booking VALUES(54,7,25, 1,'2020-02-26','2020-02-28'); 
+INSERT INTO Booking VALUES(55,7,26, 2,'2020-02-27','2020-02-28'); 
+INSERT INTO Booking VALUES(56,7,27, 3,'2020-02-28','2020-03-01'); 
+INSERT INTO Booking VALUES(57,7,28, 4,'2020-02-28','2020-03-02'); 
+INSERT INTO Booking VALUES(58,7,29,11,'2020-03-01','2020-03-03'); 
+INSERT INTO Booking VALUES(59,7,30,21,'2020-03-02','2020-03-06');
+INSERT INTO Facility VALUES (1,'Pooltable')
+INSERT INTO Facility VALUES (2,'Pool')
+INSERT INTO Facility VALUES (3,'Bar')
+INSERT INTO Facility VALUES (4,'Tabletennis table')
+INSERT INTO HotelUseOfFacilities VALUES ( 1,1,1,'Recreational room')
+INSERT INTO HotelUseOfFacilities VALUES ( 2,1,2,'Bathhouse')
+INSERT INTO HotelUseOfFacilities VALUES ( 3,1,3,'Foye')
+INSERT INTO HotelUseOfFacilities VALUES ( 4,1,4,'Recreational room')
+INSERT INTO HotelUseOfFacilities VALUES ( 5,2,1,'Recreational room')
+INSERT INTO HotelUseOfFacilities VALUES ( 6,2,2,'Bathhouse')
+INSERT INTO HotelUseOfFacilities VALUES ( 7,2,3,'Foye')
+INSERT INTO HotelUseOfFacilities VALUES ( 8,2,4,'Recreational room')
+INSERT INTO HotelUseOfFacilities VALUES ( 9,3,1,'Recreational room')
+INSERT INTO HotelUseOfFacilities VALUES (10,3,2,'Bathhouse')
+INSERT INTO HotelUseOfFacilities VALUES (11,3,3,'Foye')
+INSERT INTO HotelUseOfFacilities VALUES (12,3,4,'Recreational room')
+INSERT INTO HotelUseOfFacilities VALUES (13,4,1,'Recreational room')
+INSERT INTO HotelUseOfFacilities VALUES (14,4,2,'Bathhouse')
+INSERT INTO HotelUseOfFacilities VALUES (15,4,3,'Foye')
+INSERT INTO HotelUseOfFacilities VALUES (16,4,4,'Recreational room')
+INSERT INTO HotelUseOfFacilities VALUES (17,5,1,'Recreational room')
+INSERT INTO HotelUseOfFacilities VALUES (18,5,2,'Bathhouse')
+INSERT INTO HotelUseOfFacilities VALUES (19,5,3,'Foye')
+INSERT INTO HotelUseOfFacilities VALUES (20,5,4,'Recreational room')
+INSERT INTO HotelUseOfFacilities VALUES (21,6,1,'Recreational room')
+INSERT INTO HotelUseOfFacilities VALUES (22,6,2,'Bathhouse')
+INSERT INTO HotelUseOfFacilities VALUES (23,6,3,'Foye')
+INSERT INTO HotelUseOfFacilities VALUES (24,6,4,'Recreational room')
+INSERT INTO HotelUseOfFacilities VALUES (25,7,1,'Recreational room')
+INSERT INTO HotelUseOfFacilities VALUES (26,7,2,'Bathhouse')
+INSERT INTO HotelUseOfFacilities VALUES (27,7,3,'Foye')
+INSERT INTO HotelUseOfFacilities VALUES (28,7,4,'Recreational room')
